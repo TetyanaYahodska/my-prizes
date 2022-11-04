3d0aa2b15ebf7e15dbe4ed378b2f0051e7d71ffd
Tetiana Yahodska <tyahodska@atlassian.com>
2022-11-04 12:17:26 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/MyPrizesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrizesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPrizesApplication.class, args);
+	}
+
+}
