f1b51b1f6921c6f0dc44e724c05f1deebc43411d
tyahodska <tyahodska@atlassian.com>
2022-11-27 20:18:27 +0100

    3 Created service and repository

---
+++ src/main/java/com/example/demo/entity/UserController.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+//
+//    @RequestMapping("/users")
+//    public List<User> getAllUsers(){
+//        return userService.getAllUsers();
+//    }
+//
+//    @RequestMapping("/users/{id}")
+//    public User getUsers(@PathVariable int id){
+//        return userService.getUsers(id);
+//    }
+//
+//    @PostMapping( value = "/users")
+//    public void addUsers(@RequestBody User user){
+//        userService.addUsers(user);
+//    }
+//    @PutMapping(value = "/users/{id}")
+//    public void updateUsers( @PathVariable int id, @RequestBody User user){
+//        userService.updateUsers(user);
+//    }
+//    @DeleteMapping(value = "/users/{id}")
+//    public void deleteUsers( @PathVariable int id){
+//        userService.deleteUsers(id);
+//    }
+
+}
+
+
+
---
+++ src/main/java/com/example/demo/entity/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public class UserRepository extends CrudRepository <User, Integer>{
+}
