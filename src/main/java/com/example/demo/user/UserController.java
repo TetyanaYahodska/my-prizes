1725f12320671b1a5e2630323732faf1c82dff7c
tyahodska <tyahodska@atlassian.com>
2022-11-27 20:37:41 +0100

    1 Fixed issue

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,32 +1,7 @@
 package com.example.demo.user;
 
 public class UserController {
-
-//    @Autowired
-//    private UserService userService;
-////
-//    @RequestMapping("/users")
-//    public List<User> getAllUsers(){
-//        return userService.getAllUsers();
-//    }
-//
-//    @RequestMapping("/users/{id}")
-//    public User getUsers(@PathVariable int id){
-//        return userService.getUsers(id);
-//    }
-//
-//    @PostMapping( value = "/users")
-//    public void addUsers(@RequestBody User user){
-//        userService.addUsers(user);
-//    }
-//    @PutMapping(value = "/users/{id}")
-//    public void updateUsers( @PathVariable int id, @RequestBody User user){
-//        userService.updateUsers(user);
-//    }
-//    @DeleteMapping(value = "/users/{id}")
-//    public void deleteUsers( @PathVariable int id){
-//        userService.deleteUsers(id);
-//    }
+    //ToDo methods for manage users
 
 }
 
